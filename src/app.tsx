386f43f75988a231a5d0094df7a87a567fffafed
rogervalentim <valentim.roger33@gmail.com>
2024-05-28 22:14:14 -0300

    feat: add sigin component

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,14 @@
+import { Route, Routes } from "react-router-dom";
 import { Login } from "./components/login";
+import { Signin } from "./components/sigin";
 
 export function App() {
   return (
-   <Login />
+    <>
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/signin" element={<Signin />} />
+    </Routes> 
+    </>
   )
 }
--- src/components/sigin.tsx
+++ src/components/sigin.tsx
@@ -1,8 +1,8 @@
-import { LogIn, Mail, Lock } from "lucide-react";
+import { ClipboardPen, Mail, Lock } from "lucide-react";
 import programmer from "../assets/programmer.png";
 import { Link } from "react-router-dom";
 
-export function Login() {
+export function Signin() {
   return (
     <section className="w-full grid sm:grid-cols-1 lg:grid-cols-2">
       <div className="hidden lg:flex">
@@ -12,12 +12,10 @@ export function Login() {
         <div className="bg-[#212121] w-full h-screen lg:w-[572px] lg:h-[512px] rounded-[4px] flex justify-center flex-col items-center">
           <div>
             <div className="flex items-center gap-[10px]">
-              <span><LogIn className="text-white" size={24} /></span>
-              <h2 className="text-white text-2xl font-semibold">Faça seu login</h2>
+              <span><ClipboardPen  className="text-white" size={24} /></span>
+              <h2 className="text-white text-2xl font-semibold">Cadastre-se</h2>
             </div>
-            <div className="flex flex-col">
-              <p className="text-white mt-[5px]">Entre com suas informações de cadastro.</p>
-            </div>
+           
             <form className="flex flex-col pt-3">
               <label className="text-white font-medium">E-mail</label>
               <div className="relative mt-[5px] group">
@@ -41,15 +39,9 @@ export function Login() {
                   <Lock size={20} className="text-[#4F4F4F] group-focus-within:text-manz-500" />
                 </div>
               </div>
-              <div className="flex justify-between w-[335px] pt-5">
-                <div className="flex gap-[5px]">
-                  <input type="checkbox" className=" bg-[#2F2F2F] border-[#4E4D4D]" />
-                  <p className="text-white">Lembre-me</p>
-                </div>
-                <a href="/" className="text-manz-600 hover:text-manz-500 font-semibold hover:underline">Esqueci minha senha</a>
-              </div>
-              <button className="bg-gradient-to-l from-manz-500 to-manz-600 hover:from-manz-600 hover:to-manz-700 mt-4 text-manz-950 font-semibold w-[335px] h-[51px] rounded-[4px] text-lg">ENTRAR</button>
-              <Link to="/signin" className="text-manz-600 hover:text-manz-500 w-[335px] text-center pt-4 hover:underline">Não tem uma conta? <span className="font-medium">Registre-se</span></Link>
+              
+              <button className="bg-gradient-to-l from-manz-500 to-manz-600 hover:from-manz-600 hover:to-manz-700  text-manz-950 font-semibold w-[335px] h-[51px] rounded-[4px] mt-6 text-lg">CADASTRE-SE</button>
+              <Link to="/" className="text-manz-600 hover:text-manz-500 w-[335px] text-center pt-4 hover:underline">Já tem uma conta? <span className="font-medium">Clique aqui</span></Link>
             </form>
           </div>
         </div>
