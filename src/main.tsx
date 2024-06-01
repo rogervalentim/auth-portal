527fe873a229c95b5ad0d1cd19f341c49a6e2e6b
rogervalentim <valentim.roger33@gmail.com>
2024-05-31 23:41:21 -0300

    chore: add react-query and tailwind merge

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,20 @@ import './index.css'
 import { App } from './app'
 import { HashRouter } from 'react-router-dom'
 import { Toaster } from 'sonner'
+import { AuthProvider } from './context/auth-context'
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <HashRouter>
+    <QueryClientProvider client={queryClient}>
+    <AuthProvider>
     <Toaster richColors />
     <App />
+    </AuthProvider>
+    </QueryClientProvider>
     </HashRouter>
   </React.StrictMode>,
 )
